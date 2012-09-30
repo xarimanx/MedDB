a8d76445f92b38929d15f3f0fa3b59d94c63e2f3
Ariman <xarimanx@gmail.com>
2012-09-30 12:23:51 +0300

    app init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MedDb::Application.config.secret_token = '4d3d203a6e3ac7379fa575676830f8d34c314d40b5ce897e48b1a5a365f962b6851c5ea249b6973b159c852cbab1e7b9fbf276d6ccef2727dd26a619e151fd1a'
